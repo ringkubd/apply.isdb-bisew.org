a47e0a405d5bb37c19a05b9a6591a3a83a3932b3
Anwar Jahid <ajr.jahid@gmail.com>
2022-01-13 11:50:34 +0600

    Changes-1.1

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout";
 import {useEffect} from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
     useEffect(() => {
@@ -12,6 +13,9 @@ function MyApp({ Component, pageProps }) {
     });
     return (
         <Layout>
+            <Head>
+                <link rel="shortcut icon" href="https://isdb-bisew.org/img/logo.png" type="image/x-icon" />
+            </Head>
             <Component {...pageProps} />
         </Layout>)
 }
